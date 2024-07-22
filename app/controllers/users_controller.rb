ce03ce454fa275a14e82bee6699cd11ac535fd9f
J03L R0J0 <rojo@analogue.com>
2024-07-22 10:22:21 -0700

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  # GET /users
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  # GET /users/:id
+  def show
+    render json: @user
+  end
+
+  # PATCH/PUT /users/:id
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/:id
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
