92cf4689277625e05c0535d90ae950bec804b51e
J03L R0J0 <rojo@analogue.com>
2024-07-22 11:42:32 -0700

    Implement non-trivial sql query for songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,6 +11,16 @@ class Song < ApplicationRecord
   validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :mp3_url, presence: true
 
+  def self.songs_by_artist_and_genre(artist_id, genre_id)
+    find_by_sql([<<-SQL, artist_id, genre_id])
+      SELECT songs.*
+      FROM songs
+      JOIN albums ON albums.id = songs.album_id
+      WHERE songs.artist_id = ? AND songs.genre_id = ?
+      ORDER BY albums.release_date
+    SQL
+  end
+
   private
 
   def generate_slug
